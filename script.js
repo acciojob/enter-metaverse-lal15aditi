297930786acbaa09d913a3227cdb53cf495cf6ad
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-09-28 19:34:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById("enterBtn").addEventListener('click', () => {
+	let element = document.getElementById("status");
+	element.innerHTML = `<h1>Entered Metaverse</h1>`;
+})
